c339804ce572b4c33326f93bba93da325346cf05
brokenstone2012 <brokenstone2012@gmail.com>
2023-12-13 13:21:24 +0800

    ff

--- start.sh
+++ start.sh
@@ -6,8 +6,6 @@ iptables -P FORWARD ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE 
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport ${BIND_PORT:-80} -j DNAT --to-destination 10.0.0.2
 
-sed -i "s/TARGET_HOST/$TARGET_HOST/g" /root/lkl/haproxy.cfg
-sed -i "s/TARGET_PORT/$TARGET_PORT/g" /root/lkl/haproxy.cfg
 sed -i "s/BIND_PORT/${BIND_PORT:-80}/g" /root/lkl/haproxy.cfg
 
 export LD_PRELOAD=/root/lkl/liblkl-hijack.so
@@ -21,5 +19,4 @@ export LKL_HIJACK_NET_GATEWAY=10.0.0.1
 export LKL_HIJACK_OFFLOAD="0x8883"
 export LKL_HIJACK_DEBUG=0
 
-chmod +x /root/lkl/test && /root/lkl/test -config=config.json
 haproxy -f /root/lkl/haproxy.cfg
